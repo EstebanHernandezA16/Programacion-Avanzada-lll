b62ce38a75d9e41915fb4dabff08ff34ca8ffa00
EstebanHernandezA16 <EstebanHernandez23201@hotmail.com>
2023-09-15 08:47:51 -0500

    Clase#7 inicio backend

---
+++ Clase 7/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/habitaciones', (req,res)=> res.send('endpoint todas las habitaciones, metodo get'))
+
+app.post('/habitaciones', (req,res)=> res.send('endpoint habitaciones, metodo post'))
+
+app.get('/habitacion', (req,res)=> res.send('endpoint habitacion, metodo get'))
+
+app.put('/habitaciones', (req,res)=> res.send('endpoint habitaciones, metodo put'))
+
+app.delete('/habitaciones', (req,res)=> res.send('endpoint habitaciones, metodo delete'))
+
+app.listen(3001, () => console.log('Listening on port 3001'))
