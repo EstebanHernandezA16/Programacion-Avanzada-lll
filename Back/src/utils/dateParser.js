107ed16c67a6c03b148d32f04c0e1fd864b0002c
Murork <EstebanHernandez23201@hotmail.com>
2023-10-26 23:50:17 -0500

    solucion fecha

--- Back/src/utils/dateParser.js
+++ Back/src/utils/dateParser.js
@@ -3,6 +3,14 @@
 
 export const dateParser =() =>{
 
+
+    let fechaIni = new Date("01/01/2022");
+    let fechaFin = new Date("12/24/2022");
+
+
+    return(fechaFin.getTime() - fechaIni.getTime()) / 1000 / 60 /60 / 24
+                                            // milisegundos   sec  min  horas
+
     //OJO, fecha Inicio y fecha Fin manda datos de tipo date
     const fIni = '30/04/2002'
     const fFin = '05/05/2002'
